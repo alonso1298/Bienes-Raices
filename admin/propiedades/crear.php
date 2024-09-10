91c89c00fa8f42a71853b9ef8de26a6a64b85e30
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-09 22:16:18 -0600

    Se crea el formulario para las propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,20 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form class="formulario">
+
+        <fieldset>
+            <legend>Información General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="text" id="precio" placeholder="Precio Propiedad">
+        </fieldset>
+
+        </form>
     </main>
 
 <?php
