ad83d439b04c916147fe308da6f9cf924dcc9c98
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-11 23:33:23 -0600

    Se crea un select con los valores de la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,6 +3,10 @@
     require '../../includes/config/database.php';
     $db = conectarDB();
 
+    //Consultar para obtener los vendedores
+    $consulta = "SELECT * FROM vendedores"; 
+    $resultado = mysqli_query($db, $consulta);
+
     // Arreglo con mendajes de errores
     $errores = [];
 
