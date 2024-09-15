ff720f3c690f6b8b2895b0b881ed07da7076cba1
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-15 02:46:56 -0600

    Se inicia con la creacion de tabala para listar las propiedades

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,8 @@
     // echo '</pre>';
     // exit; 
 
-    $resultado = $_GET['resultado'] ?? null; // El paceholder ?? null busca el valor $_GET['resultado'] y si no existe le asigna null 
+    $resultado = $_GET['resultado'] ?? null; // El paceholder ?? null busca el valor $_GET['resultado'] y si no existe le asigna null }
+
     require '../includes/funciones.php'; 
     incluirTemplate('header');
 ?>
@@ -17,6 +18,30 @@
             <p class="alerta exito">Anuncio Creado Correctamente</p>
         <?php endif; ?>   
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imgen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="/imagenes/1c843bd0672434147acd3a256e740805.jpg" class="imagen-tabla"></td>
+                    <td>$12000000</td>
+                    <td>
+                        <a href="">Eliminar</a>
+                        <a href="">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
