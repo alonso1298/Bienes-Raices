a8d8ea06f8cbb2d9af2f146e9d23e842d5fa7dcc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-16 03:43:36 -0600

    Se finaliza con la funcionalidad para eliminar propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,8 @@
 <?php
 
-    echo('<pre>');
-    var_dump($_POST);
-    echo('</pre>');
+    // echo('<pre>');
+    // var_dump($_POST);
+    // echo('</pre>');
 
     // 1- Importar la conexión a la BD 
     require '../includes/config/database.php';
@@ -23,6 +23,32 @@
     // Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null; // El paceholder ?? null busca el valor $_GET['resultado'] y si no existe le asigna null }
 
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        //Se hace de esta manera porque ese id no va a existir hata que se haga el REQUEST_METHOD ya que de lo contrario nos daria un error de undefined
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if($id) {
+
+            //Eliminar el archivo
+            $query = "SELECT imagen FROM propiedades WHERE id = {$id}";
+
+            $resultado = mysqli_query($db, $query);
+            $propiedad = mysqli_fetch_assoc($resultado);
+
+            unlink('../imagenes/' . $propiedad['imagen']);
+
+            //Eliminar la propiedad 
+            $query = "DELETE FROM propiedades WHERE id = {$id}";
+            $resultado = mysqli_query($db, $query);
+
+            if($resultado) {
+                header('location: /admin?resultado=3');
+            }
+        }
+    }
+
     // Incluye un template
     require '../includes/funciones.php'; 
     incluirTemplate('header');
@@ -34,6 +60,8 @@
             <p class="alerta exito">Anuncio Creado Correctamente</p>
         <?php elseif( intval($resultado) === 2): ?>
             <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+        <?php elseif( intval($resultado) === 3): ?>
+            <p class="alerta exito">Anuncio Eliminado Correctamente</p>
         <?php endif; ?>   
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
@@ -55,14 +83,14 @@
                     <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"></td>
                     <td><?php echo $propiedad['precio']; ?></td>
                     <td>
-                        <form method="$_POST" class="w-100">
+                        <form method="POST" class="w-100">
 
                             <!--Se crea un input que no se vea que contenga toda la informacion de la propiedad-->
-                            <input type="hidden" name="id" value="Hola">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
 
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
-                        
+
                         <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
