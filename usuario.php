181b35669cf54d5538c380feb949607c028694c8
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-18 18:59:34 -0600

    Se crea una tabla para los usuarios

--- usuario.php
+++ usuario.php
@@ -1,2 +1,18 @@
 <?php
 
+// Importar la conexión
+require __DIR__ . '/includes/config/database.php';
+$db = conectarDB();
+
+// Crear un Emai y Password
+$email = 'correo@correo.com';
+$password = '123456';
+
+
+// Query para crear el usuario
+$query = "INSERT INTO usuarios (email, password) VALUES ('{$email}', '{$password}') ";
+
+echo $query;
+
+// Agregarlo a la Base de Datos 
+mysqli_query($db, $query);
