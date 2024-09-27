7d26b87076b574d694f8cbbd101329cb1c391015
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-26 19:22:37 -0600

    Creando clase propiedad utilizando active record

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,14 +1,23 @@
 <?php
 
-    require '../../includes/funciones.php'; 
+    require '../../includes/app.php'; 
+
+    use App\Propiedad;
+
+    $propiedad = new Propiedad;
+
+    echo '<pre>';
+    var_dump($propiedad);
+    echo '</pre>';
+
+    exit;
+
     $auth = estaAutenticado();
 
     if(!$auth) {
         header('Location: /');
     }
 
-    // Base de datos
-    require '../../includes/config/database.php';
     $db = conectarDB();
 
     //Consultar para obtener los vendedores
--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -4,4 +4,26 @@ namespace App;
 
 class Propiedad {
     
+    public $id;
+    public $titulo;
+    public $precio;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $creado;
+    public $vendedores_id;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->precio = $args['precio'] ?? '';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->habitaciones = $args['habitaciones'] ?? '';
+        $this->wc = $args['wc'] ?? '';
+        $this->estacionamiento = $args['estacionamiento'] ?? '';
+        $this->creado = $args['creado'] ?? '';
+        $this->vendedores_id = $args['vendedores_id'] ?? '';
+    }
 }
