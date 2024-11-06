dcbf58a181f936fe43d4b3dbea2ace65191c9365
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-11-05 22:00:29 -0600

    se añade validacion a la clase propiedades

--- classes/ActiveRecord.php
+++ classes/ActiveRecord.php
@@ -131,38 +131,12 @@ class ActiveRecord {
 
     // Validacion
     public static function getErrores() {
-        return self::$errores;
+        return static::$errores;
     }
 
     public function validar() {
-        
-        if(!$this->titulo) { // !$titulo significa que si no hay titulo o si esta vacío 
-            self::$errores[] = "Debes añadir un titulo"; // Detecta que $errores es un arreglo y la sintaxias va agregarlo en el arreglo
-        }
-        if(!$this->precio) { 
-            self::$errores[] = "Debes añadir un precio"; 
-        }
-        if(strlen( $this->descripcion ) < 50 || strlen( $this->descripcion ) > 500 ) { // Validamos que la descripción tenga al menos 50 caracteres y maximo 500 caracteres
-            self::$errores[] = "La descripción es obligatoria y debe tener al menos 50 caracteres";
-        }
-        if(!$this->habitaciones) { 
-            self::$errores[] = "El número de habitaciones es obligatorio"; 
-        }
-        if(!$this->wc) { 
-            self::$errores[] = "El número de baños es obligatorio"; 
-        }
-        if(!$this->estacionamiento) { 
-            self::$errores[] = "El número de lugares de estacionamiento es obligatorio"; 
-        }
-        if(!$this->vendedores_id) { 
-            self::$errores[] = "Elige un vendedor"; 
-        }
-
-        if(!$this->imagen) {
-            self::$errores[] = "La imagen es obligatoria";
-        }
-
-        return self::$errores;
+        static::$errores;
+        return static::$errores;
     }
 
     // Lista todos los registros
--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -31,4 +31,35 @@ class Propiedad extends ActiveRecord{
         $this->creado = date('Y/m/d');
         $this->vendedores_id = $args['vendedores_id'] ?? '';
     }
+
+    public function validar() {
+        
+        if(!$this->titulo) { // !$titulo significa que si no hay titulo o si esta vacío 
+            self::$errores[] = "Debes añadir un titulo"; // Detecta que $errores es un arreglo y la sintaxias va agregarlo en el arreglo
+        }
+        if(!$this->precio) { 
+            self::$errores[] = "Debes añadir un precio"; 
+        }
+        if(strlen( $this->descripcion ) < 50 || strlen( $this->descripcion ) > 500 ) { // Validamos que la descripción tenga al menos 50 caracteres y maximo 500 caracteres
+            self::$errores[] = "La descripción es obligatoria y debe tener al menos 50 caracteres";
+        }
+        if(!$this->habitaciones) { 
+            self::$errores[] = "El número de habitaciones es obligatorio"; 
+        }
+        if(!$this->wc) { 
+            self::$errores[] = "El número de baños es obligatorio"; 
+        }
+        if(!$this->estacionamiento) { 
+            self::$errores[] = "El número de lugares de estacionamiento es obligatorio"; 
+        }
+        if(!$this->vendedores_id) { 
+            self::$errores[] = "Elige un vendedor"; 
+        }
+
+        if(!$this->imagen) {
+            self::$errores[] = "La imagen de la propiedad es obligatoria";
+        }
+
+        return self::$errores;
+    }
 }
