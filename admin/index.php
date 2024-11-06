19a5cdcb4b6463836a8d2719be37f3af381bfe25
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-11-05 22:24:44 -0600

    Se listan los vendedores y vendedoras de la BD

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
     estaAutenticado();
 
     use App\Propiedad;
-use App\Vendedor;
+    use App\Vendedor;
 
     // Implementar un metodo para obtener todas las propiedades
     $propiedades = Propiedad::all();
@@ -15,6 +15,7 @@ use App\Vendedor;
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+        debuguear($_POST);
         //Se hace de esta manera porque ese id no va a existir hata que se haga el REQUEST_METHOD ya que de lo contrario nos daria un error de undefined
         $id = $_POST['id'];
         $id = filter_var($id, FILTER_VALIDATE_INT);
@@ -42,6 +43,8 @@ use App\Vendedor;
         <?php endif; ?>   
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+        <h2>Propiedades</h2>
+
         <table class="propiedades">
             <thead>
                 <tr>
@@ -58,7 +61,7 @@ use App\Vendedor;
                     <td><?php echo $propiedad->id; ?></td>
                     <td><?php echo $propiedad->titulo; ?></td>
                     <td> <img src="/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla"></td>
-                    <td><?php echo $propiedad->precio; ?></td>
+                    <td>$<?php echo $propiedad->precio; ?></td>
                     <td>
                         <form method="POST" class="w-100">
 
@@ -74,12 +77,38 @@ use App\Vendedor;
                 <?php endforeach ?>
             </tbody>
         </table>
+
+        <h2>Vendedores</h2>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Télefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!-- 4- Mostrar los Resultados -->
+                <?php foreach( $vendedores as $vendedor ): ?>
+                <tr>
+                    <td><?php echo $vendedor->id; ?></td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="admin/vendedores/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
     </main>
 
 <?php
-
-    // 5- Opcional, cerrar la conexión 
-    mysqli_close($db);
-
     incluirTemplate('footer'); 
 ?>
