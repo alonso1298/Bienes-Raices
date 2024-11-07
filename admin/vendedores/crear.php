54d5c0b15f3a3832625aad1c520e78a49adaf845
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-11-07 13:41:35 -0600

    Se agrega el formulario para registrar a nuevos vendedores

--- admin/vendedores/crear.php
+++ admin/vendedores/crear.php
@@ -21,7 +21,7 @@ incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion">
-        <h1>Registrar Vendedores</h1>
+        <h1>Registrar Vendedor(a)</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
 
